57a36668e4057065c9311c1404e51d2e24d97f8c
riston <est.risto@gmail.com>
2011-08-25 21:13:33 +0300

    Moved the data to global array.

--- PirateBay/index.php
+++ PirateBay/index.php
@@ -5,24 +5,24 @@ require_once './cleaner.class.php';
 
 define('CACHE_DIR', './cache');
 $url = 'https://thepiratebay.org/top/201';
-$cachePirateBayPage = './cache/pirate.cache';
+$cachePirateBayPage = CACHE_DIR .'/pirate.cache';
 $cacheLife = 360; // In seconds
 
-$modifiedTime = @filemtime($cachePirateBayPage);
-if (!$modifiedTime && (time() - $modifiedTime >= $cacheLife)) {
-	// Make cache file first then start handling
-	printf("Downloading the file\n");
-	writeToFile($cachePirateBayPage, file_get_contents($url));
-}
 
-$html = file_get_html($cachePirateBayPage);
-$imdb = new Imdb();
 
 function writeToFile($filename, $content) {
 	$fp = fopen($filename, 'w');
 	fwrite($fp, $content);
 	fclose($fp);
+}
 
+function cachePageFromUrl($url, $cacheFile, $cacheLife) {
+	$modifiedTime = @filemtime($cacheFile);
+	if (!$modifiedTime && ((time() - $modifiedTime) <= $cacheLife)) {
+		// Make cache file first then start handling
+		printf("Downloading the file\n");
+		writeToFile($cacheFile, file_get_contents($url));
+	}
 }
 
 function cacheIMDBMovie($imdb, $movieName) {
@@ -48,62 +48,49 @@ function imgDisplay($movieArray) {
 	}
 	return sprintf('<img src="%s" width="101" height="150" />', $src);
 }
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-<head>
-	<title>PirateBay Movie top 100</title>
-	<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" charset="utf-8"/>
-	<link rel="stylesheet" href="css/site.css" type="text/css" media="screen" charset="utf-8"/>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script type="text/javascript" charset="utf-8">
-		$(document).ready(function() {
-			
-		});
-	</script>
-</head>
-<body>
-	
 
-<?php  foreach ($html->find('.detName a') as $element): ?>
-	<?php 
-	$cleaner = new PirateBay_Cleaner(); 
-	$cleaner->filterRunner($element->title);
-	?>
-	<div class="movie">
-		<!-- IMDB data view here -->
-		<?php $movieArray = cacheIMDBMovie($imdb, $cleaner->getMovieName()); ?>
-		<?php if (!isset($movieArray['error'])): ?>
-			<a href="<?php echo $movieArray['imdb_url']; ?>" class="movie-title"><?php echo $movieArray['title']; ?></a>	
-			<p>
-				<?php echo imgDisplay($movieArray); ?>
-				<?php echo $movieArray['storyline']; ?>
-			</p>
-			<p>
-				Year: <span><?php echo $movieArray['year']; ?></span>
-			</p>
-			<p>
-				Rating: <span><?php echo $movieArray['rating']; ?></span>, Votes: <span><?php echo $movieArray['votes']; ?></span>
-			</p>
-			<ul class="genres">
-				<?php foreach ($movieArray['genres'] as $genre): ?>
-					<li><a href="http://www.imdb.com/genre/<?php echo $genre; ?>">
-						<?php echo $genre; ?></a></li>
-				<?php endforeach; ?>
-			</ul>
-			<hr />
-		<?php endif; ?>
-		
-		<!-- Piratebay information -->
-		<h3><?php echo $cleaner->getMovieName(); ?></h3>
-		<p>The keywords with movie from Pirate Bay</p>
-		<?php foreach ($cleaner->getKeywords() as $keyword): ?>
-		<ul>
-			<li><?php echo $keyword; ?></li>
-		</ul>
-		<?php endforeach; ?>
-	</div>
-<?php endforeach; ?>
-</body>
-</html>
+function getPirateBayInfo($html) {
+	$titles = array();
+	foreach ($html->find('.detName .detLink') as $element) {
+		$titles[] = $element->title;
+	}
+	
+	$links = array();
+	foreach ($html->find('a[title=Download this torrent]') as $link) {
+		$links[] = $link->href ." \n";
+	}
+	
+	$magnets = array();
+	foreach ($html->find('a[title$=using magnet]') as $link) {
+		$magnets[] = $link->href ." \n";
+	}
+
+	$movieArray = array();
+	for ($i = 0; $i < count($titles); $i++) {
+		$movieArray[] = array('title' => $titles[$i], 'link' => $links[$i], 'magnet' => $magnets[$i]);
+	}	
+	return $movieArray;
+}
+
+cachePageFromUrl($url, $cachePirateBayPage, $cacheLife);
+$html = new simple_html_dom();
+$html->load_file($cachePirateBayPage);
+$imdb = new Imdb();
+
+$pirateMovieArray = getPirateBayInfo($html);
+$movieArray = array();
+foreach ($pirateMovieArray as $movie) {
+	$cleaner = new PirateBay_Cleaner();
+	$cleaner->filterRunner($movie['title']);
+	
+	$movieRow = array();
+	$movieRow = cacheIMDBMovie($imdb, $cleaner->getMovieName());
+	$movieRow['torrent_title'] = $movie['title'];
+	$movieRow['torrent_link'] = $movie['link'];
+	$movieRow['torrent_magnet'] = $movie['magnet'];
+	$movieRow['torrent_keywords'] = $cleaner->getKeywords();
+	$movieArray[] = $movieRow;
+}
+array_unique($movieArray);
+require_once 'page.inc.php';
+?>
---
+++ PirateBay/page.inc.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+	<title>PirateBay Movie top 100</title>
+	<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" charset="utf-8"/>
+	<link rel="stylesheet" href="css/site.css" type="text/css" media="screen" charset="utf-8"/>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="js/jcarousellite_1.0.1.min.js"></script>
+	<script type="text/javascript" charset="utf-8">
+		$(document).ready(function() {
+			$('.torrent-button').click(function() {
+				$(this).prev('.torrent').fadeToggle("slow", "swing");
+			});
+		});
+	</script>
+</head>
+<body>
+<div id="container">
+<?php  foreach ($movieArray as $movie): ?>
+	<div class="movie">
+		<!-- IMDB data view here -->
+		<?php if (!isset($movie['error'])): ?>
+			<a href="<?php echo $movie['imdb_url']; ?>" class="movie-title"><?php echo $movie['title']; ?></a>
+			<p>
+				<?php echo imgDisplay($movie); ?>
+				<?php echo $movie['storyline']; ?>
+			</p>
+			<p>
+				Year: <span><?php echo $movie['year']; ?></span>
+			</p>
+			<p>
+				Rating: <span><?php echo $movie['rating']; ?></span>, 
+				Votes: <span><?php echo $movie['votes']; ?></span>
+			</p>
+			<ul class="genres">
+				<?php foreach ($movie['genres'] as $genre): ?>
+					<li><a href="http://www.imdb.com/genre/<?php echo $genre; ?>">
+						<?php echo $genre; ?></a></li>
+				<?php endforeach; ?>
+			</ul>
+
+			<hr />
+		<?php endif; ?>
+		
+		<!-- Piratebay information -->
+		<div class="torrent">
+			<h3><?php echo $movie['torrent_title']; ?></h3>
+			<a href="<?php echo $movie['torrent_link'] ?>">Torrent</a>
+			<a href="<?php echo $movie['torrent_magnet'] ?>">Magnet</a>
+			<p>The keywords with movie from Pirate Bay</p>
+			<?php foreach ($movie['torrent_keywords'] as $keyword): ?>
+			<ul class="genres">
+				<li><a href="#"><?php echo $keyword; ?></a></li>
+			</ul>
+			<?php endforeach; ?>
+		</div>
+		<div class="torrent-button"><a href="#<?php echo $movie['torrent_title']; ?>">Show torrent data</a></div>
+	</div>
+	
+<?php endforeach; ?>
+</div>
+</body>
+</html>
